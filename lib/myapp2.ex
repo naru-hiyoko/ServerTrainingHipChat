0201240dd6796d4f18f6bae53c44d8d30b3286f7
Atsushi Narusawa <atsushi.narusawa@access-company.com>
2018-05-07 15:31:17 +0900

    HipChat にメッセージ送信迄

--- lib/myapp2.ex
+++ lib/myapp2.ex
@@ -16,8 +16,22 @@ defmodule Myapp2 do
     :world
   end
 
+  alias Hipchat.Httpc.Response
+  alias Hipchat.V2.Rooms
+
   def main(_) do
-    IO.puts("hello naru")
+    room_id = "4575451"
+    access_token = load_token()
+    client = Hipchat.ApiClient.new(access_token)
+    msg = "test dayo"
+    Rooms.send_message(client, room_id, %{message: msg})
   end
+
+  def load_token() do
+    Path.expand("/private/tmp/myapp2/hipchat_credidental")
+    |> File.read!()
+    |> String.trim()
+  end
+
   
 end
